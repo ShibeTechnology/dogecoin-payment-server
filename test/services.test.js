0255b315a02fb5de7138ead55bb358e68677b84e
Patrick Lodder <patricklodder@users.noreply.github.com>
2021-12-02 21:08:03 -0400

    cleanup: test the result instead of printing it

--- test/services.test.js
+++ test/services.test.js
@@ -13,10 +13,6 @@ const {
   generateTx,
 } = require('./helpers');
 
-const initPaymentService = (expiry) => {
-    return new PaymentService(networks.regtest, expiry);
-}
-
 describe('payment service', () => {
     const keyPairA = generateKeyPair()
     const keyPairB = generateKeyPair()
@@ -24,16 +20,13 @@ describe('payment service', () => {
     const rs = constructRS(locktime, keyPairA, keyPairB)
     const p2sh = generateP2SH(rs)
     const alice = bitcoinjs.payments.p2pkh({ pubkey: keyPairA.publicKey, network: networks.regtest })
-    let tx = generateTx(p2sh, 100000000, alice, keyPairA)
-    let ps = initPaymentService(1159)
-    let result = ''
+    const tx = generateTx(p2sh, 100000000, alice, keyPairA)
+    const ps = new PaymentService(networks.regtest, 1159);
+
     it('should be a goodPsbtHex', function(done) {
-        console.log(tx)
-        let res = generatePsbtHex(tx, p2sh, 100000000, alice, keyPairA, rs)
-        console.log(res)
-        console.log(ps)
-        result = ps.checkPSBT(keyPairA.publicKey.toString('hex'), res.toHex())
-        console.log(result)
+        const psbt = generatePsbtHex(tx, p2sh, 100000000, alice, keyPairA, rs)
+        const result = ps.checkPSBT(keyPairA.publicKey.toString('hex'), psbt.toHex())
+        assert(result.isOk(), result.errors.join("\n"))
         done()
     })
 });
