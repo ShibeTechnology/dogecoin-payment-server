28e129b7ead537d51f5211c93d323084280a59de
bluezr <bluezr@dogecoin.com>
2022-09-20 00:21:32 -0700

    bip44: update chain params, add simple test case

--- src/networks.js
+++ src/networks.js
@@ -1,3 +1,26 @@
+const mainnet = {
+  messagePrefix: '\x18Dogecoin Signed Message:\n',
+  bech32: 'tdge',
+  bip32: {
+    public: 0x02facafd,
+    private: 0x02fac398
+  },
+  pubKeyHash: 0x1E,
+  scriptHash: 0x16,
+  wif: 0x9E,
+};
+
+const testnet = {
+  messagePrefix: '\x18Dogecoin Signed Message:\n',
+  bech32: 'tdge',
+  bip32: {
+    public: 0x0432a9a8,
+    private: 0x0432a243
+  },
+  pubKeyHash: 0x71,
+  scriptHash: 0xc4,
+  wif: 0xf1,
+};
 
 const regtest = {
   messagePrefix: '\x18Dogecoin Signed Message:\n',
@@ -8,9 +31,11 @@ const regtest = {
   },
   pubKeyHash: 0x6f,
   scriptHash: 0xc4,
-  wif: 0xef
-}
+  wif: 0xef,
+};
 
 module.exports = {
+  mainnet,
+  testnet,
   regtest
-}
+};
